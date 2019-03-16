0821025dd5dd60accfe23ac8ced0b6f77ab236fa
hongdp <hongdp@gmail.com>
2019-03-16 16:19:08 -0700

    remove unused code

--- agents/agent.py
+++ agents/agent.py
@@ -13,8 +13,3 @@ class Agent():
     def act(self, feature):
         raise NotImplementedError
 
-def get_agent(agent_name):
-    if env_name == 'const':
-        return ConstConfig()
-    elif env_name == 'clinical_dosing':
-        return ClinicalDosingConfig()
