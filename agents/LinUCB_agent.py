072635f62256feb9203318188340c27d674fb543
jxzhm-728 <mamaimai666@gmail.com>
2019-03-16 20:07:41 -0700

    add nitial history and payoff

--- agents/LinUCB_agent.py
+++ agents/LinUCB_agent.py
@@ -5,7 +5,8 @@ from .agent import Agent, Actions
 class LinUCBAgent(Agent):
     def __init__(self, config, vocab):
         self.vocab = vocab
-
+        self.history = []
+        self.total = 0
         pass
 
     def act(self, feature):
