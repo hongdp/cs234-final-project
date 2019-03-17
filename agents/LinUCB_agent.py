e836558069409364cdedcfffad4b7f04f75dfaf7
jxzhm-728 <mamaimai666@gmail.com>
2019-03-16 19:49:58 -0700

    start LinUCB

---
+++ agents/LinUCB_agent.py
@@ -0,0 +1,16 @@
+import numpy as np
+from .agent import Agent, Actions
+
+
+class LinUCBAgent(Agent):
+    def __init__(self, config, vocab):
+        self.vocab = vocab
+
+        pass
+
+    def act(self, feature):
+        pass
+
+    def feedback(self, feature, reward):
+
+        pass
--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,6 @@
 from .clinical_dosing_agent import ClinicalDosingAgent
 from .const_agent import ConstAgent
+from .LinUCB_agent import LinUCBAgent
 from .agent import Actions
 
 def get_agent(agent_name, config, vocab):
@@ -7,3 +8,5 @@ def get_agent(agent_name, config, vocab):
         return ConstAgent(config, vocab)
     elif agent_name  == 'clinical_dosing':
         return ClinicalDosingAgent(config, vocab)
+    elif agent_name == 'LinUCB':
+        return LinUCBAgent(config, vocab)
